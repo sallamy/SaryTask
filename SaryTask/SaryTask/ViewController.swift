d133fd03e1bcb8a9eebdf95dbe04195ce612b1d3
sala7 <mohamed.sallam@contactcars.com>
2022-05-01 03:50:17 +0200

    first commit add initial project

---
+++ SaryTask/SaryTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SaryTask
+//
+//  Created by concarsadmin on 5/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
